4dee640f360a160761876cd511da312c78efa88a
Jeroen Van den Broeck <jeroen.vandenbroeck@realdolmen.com>
2022-11-17 21:05:28 +0100

    Bugfix EnemyDisplay Health

---
+++ src/projects/DiceGladiator/DiceGladiator.Tests/Domain/Models/EnemyGenerationTests.cs
@@ -0,0 +1,22 @@
+﻿using DiceGladiator.Domain.Models;
+
+namespace DiceGladiator.Tests.Domain.Models
+{
+	public class EnemyGenerationTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void MaxHealth()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				var enemy = new Enemy();
+				Assert.That(enemy.Health, Is.LessThan(40));
+			}
+		}
+	}
+}
---
+++ src/projects/DiceGladiator/DiceGladiator.Tests/Domain/Services/GameServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using DiceGladiator.Domain.Models;
+using DiceGladiator.Domain.Services;
+
+namespace DiceGladiator.Tests.Domain.Services
+{
+	public class GameServiceTests
+	{
+		private const int ScoreLimit = 1000;
+		private GameService _gameService;
+		private List<Player> _players = new List<Player> { new Player { Name = "Player1" } };
+
+		[SetUp]
+		public void Setup()
+		{
+			_gameService = new GameService();
+			_gameService.Start(_players, ScoreLimit);
+		}
+
+		[Test]
+		public void NextEnemyMaxHealth()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				_gameService.NextEnemy(_players.FirstOrDefault());
+				Assert.That(_gameService.CurrentEnemy.Health, Is.LessThan(40));
+			}
+		}
+	}
+}
