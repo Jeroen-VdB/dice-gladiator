067c969c9eb664b61353982ebf6c7876c2546fdd
Jeroen Van den Broeck <jeroen.vandenbroeck@realdolmen.com>
2022-11-17 22:45:57 +0100

    Cascading GameService

--- src/projects/DiceGladiator/DiceGladiator.Tests/Domain/Services/GameServiceTests.cs
+++ src/projects/DiceGladiator/DiceGladiator.Tests/Domain/Services/GameServiceTests.cs
@@ -6,32 +6,35 @@ namespace DiceGladiator.Tests.Domain.Services
 	public class GameServiceTests
 	{
 		private const int ScoreLimit = 1000;
-		private GameService _gameService;
-		private List<Player> _players = new List<Player> { new Player { Name = "Player1", Score = 10 } };
 
 		[SetUp]
 		public void Setup()
 		{
-			_gameService = new GameService();
-			_gameService.Start(_players, ScoreLimit);
 		}
 
 		[Test]
 		public void NextEnemyMaxHealth()
 		{
+			GameService gameService = new GameService();
+			List<Player> _players = new List<Player> { new Player { Name = "Player1", Score = 10 } };
+			gameService.Start(_players, ScoreLimit);
+
 			for (int i = 0; i < 1000; i++)
 			{
-				_gameService.NextEnemy(_players.FirstOrDefault());
-				Assert.That(_gameService.CurrentEnemy.Health, Is.LessThan(40));
+				gameService.NextEnemy(_players.FirstOrDefault());
+				Assert.That(gameService.CurrentEnemy.Health, Is.LessThan(40));
 			}
 		}
 
 		[Test]
 		public void ResumePreviousGameTest()
 		{
+			GameService gameService = new GameService();
+			List<Player> _players = new List<Player> { new Player { Name = "Player1", Score = 10 } };
+			gameService.Start(_players, ScoreLimit);
 			var newSession = new GameService();
 
-			newSession.ResumePreviousGame(_gameService);
+			newSession.ResumePreviousGame(gameService);
 
 			Assert.That(newSession.Players.First().Name, Is.EqualTo("Player1"));
 			Assert.That(newSession.Players.First().Score, Is.EqualTo(10));
